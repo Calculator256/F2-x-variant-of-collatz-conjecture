8f3dbf8acc0135a1d8d4b3f2f092e9058eeac99f
Calculator256 <167644854+Calculator256@users.noreply.github.com>
2026-06-08 23:26:00 +0300

    Update CollatzOperations.cpp

--- CollatzOperations.cpp
+++ CollatzOperations.cpp
@@ -4,14 +4,29 @@ using namespace std;
 #define ll long long
 #define ull unsigned ll
 
-ull CollatzRegularOp(ull pol){
+ull CollatzRegrOp(ull pol){
   if(pol&1 == 0){
     return pol>>1; // dividing by x
   }
   return (pol<<1)^(pol^1) // multiplying by x+1 and adding 1
-  return ((pol^bit_floor(pol))<<1)^pol;
 }
 
 ull CollatzFastOp(ull pol){
   return ((pol^bit_floor(pol))<<1)^pol; // multiplying pol by x+1 and deleting the leading term, proven to be eqiuvelent(but faster bc it skips diviing) to the reguler F2[x] collatz operation
+}
+
+int CollatzRegCollapseTime(ull pol){
+  int ans = 0;
+  while(pol!=1){
+    pol = CollatzRegrOp(pol);
+    ans++;
+  }
+}
+
+int CollatzFastCollapseTime(ull pol){
+  int ans = 0;
+  while(pol!=1){
+    pol = CollatzFastOp(pol);
+    ans++;
+  }
 }
